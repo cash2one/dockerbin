75d56cc6521498ddc663bebee1fba833bdf11ce4
zerocoolys <vbzerocool@gmail.com>
2015-06-05 13:52:18 +0800

    新增docker命令

---
+++ mongodb/run.sh
@@ -0,0 +1,2 @@
+docker run -d --name mongodb -p 27017:27017 -p 28017:28017 -e MONGODB_PASS="3edczaq1" tutum/mongodb
+
---
+++ redis/run.sh
@@ -0,0 +1,1 @@
+docker run -d --name redis -p 16379:6379 -e REDIS_PASS="3edcxsw2" tutum/redis
