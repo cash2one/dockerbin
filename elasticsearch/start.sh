9d62f7550070f9286f48793124b883b8607fd3a8
YouSheng <zerocoolys@users.noreply.github.com>
2015-06-13 11:26:47 +0800

    Update start.sh

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ES_HOME=/usr/local/elasticsearch
+
 if [ ! -e /conf/elasticsearch.* ]; then
   cp $ES_HOME/config/elasticsearch.yml /conf
 fi
@@ -8,11 +10,7 @@ if [ ! -e /conf/logging.* ]; then
   cp $ES_HOME/config/logging.yml /conf
 fi
 
-OPTS="-Des.path.conf=/conf \
-  -Des.path.data=/data \
-  -Des.path.logs=/data \
-  -Des.transport.tcp.port=9300 \
-  -Des.http.port=9200"
+OPTS=""
 
 if [ -n "$CLUSTER" ]; then
   OPTS="$OPTS -Des.cluster.name=$CLUSTER"
@@ -29,14 +27,6 @@ fi
 if [ -n "$PUBLISH_AS" ]; then
   OPTS="$OPTS -Des.transport.publish_host=$(echo $PUBLISH_AS | awk -F: '{print $1}')"
   OPTS="$OPTS -Des.transport.publish_port=$(echo $PUBLISH_AS | awk -F: '{if ($2) print $2; else print 9300}')"
-fi
-
-if [ -n "$PLUGINS" ]; then
-  for p in $(echo $PLUGINS | awk -v RS=, '{print}')
-  do
-    echo "Installing the plugin $p"
-    $ES_HOME/bin/plugin --install $p
-  done
 fi
 
 echo "Starting Elasticsearch with the options $OPTS"
