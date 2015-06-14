8d7df1ee12521237ba9cab803b3925026dfd4254
JasonYou <vbzerocool@gmail.com>
2015-06-14 21:09:41 +0800

    注释掉 错误参数

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -1,2 +1,4 @@
-docker run -d --name mongodb -p 27017:27017 -p 28017:28017 -e MONGODB_PASS="3edczaq1" tutum/mongodb
+#!/bin/bash
 
+
+docker run -d --name HUIYAN_MONGODB -p 37017:27017 -p 38017:28017 -e MONGODB_PASS=3edczaq1 -v /mongodb/data/:/data huiyan_mongodb /run.sh
